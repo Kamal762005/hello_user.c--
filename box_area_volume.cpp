675d57a5611163ed59acbef9a11fabfcec49d468
Kamal762005 <158756028+Kamal762005@users.noreply.github.com>
2024-02-16 20:35:30 +0530

    Update box_area_volume.cpp

--- box_area_volume.cpp
+++ box_area_volume.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
-#include "boxArea.h"
 #include "boxVolume.h"
+#include "boxArea.h"
 using namespace std;
-int main(){
+int main()
+{
     float l,b,h;
-    cout<<"enter the length, breadth, height : ";
-    cin>>l>>b>>h;
+    cout<<"Enter length of box: ";
+    cin>>l;
+    cout<<"Enter width of box: ";
+    cin>>b;
+    cout<<"Enter height of box: ";
+    cin>>h;
     boxArea(l,b,h);
-    cout<<"enter the length, breadth, height : ";
-    cin>>l>>b>>h;
     boxVolume(l,b,h);
+    return 0;
 }
